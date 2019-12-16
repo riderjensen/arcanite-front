d0707e646442c4af0aca7d893256ee7f974b192d
rider <riderjensen@gmail.com>
2019-12-15 18:02:55 -0700

    Adding in navigation

--- src/App.js
+++ src/App.js
@@ -2,11 +2,14 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Link } from 'react-router-dom'
 import './App.css';
 
+import Navigation from './components/navigation/Navigation';
+
 import Routes from './Routes';
 
 function App() {
   return (
     <Router>
+      <Navigation />
       <Routes />
       <div className="App">
       </div>

ea47325ced647f924d3c0ee05d3ac195368b53c6
rider <riderjensen@gmail.com>
2019-12-17 01:05:28 -0700

    small change on cards

--- src/components/users/User.js
+++ src/components/users/User.js
@@ -5,10 +5,15 @@ import './User.css';
 function User(props) {
     return (
         <div className="user-card">
-            <img src={props.picture.large} alt={props.name.first} />
-            <h3>{props.name.first} {props.name.last}</h3>
-            <p>Member for {props.registered.age} {parseInt(props.registered.age) > 1 ? "years" : "year"}</p>
-            <a href={"mailto:" + props.email}>Ask {props.name.first} about {props.gender === "female" ? "her" : "his"} experience</a>
+            <div className="user-card-image-container">
+                <img src={props.picture.large} alt={props.name.first} />
+            </div>
+            <div className="user-card-information">
+                <h3>{props.name.first} {props.name.last}</h3>
+                <p>{props.location.country}</p>
+                <p>Member for {props.registered.age} {parseInt(props.registered.age) > 1 || parseInt(props.registered.age) === 0  ? "years" : "year"}</p>
+                <a href={"mailto:" + props.email}>Ask {props.name.first} about {props.gender === "female" ? "her" : "his"} experience</a>
+            </div>
         </div>
     )
 }

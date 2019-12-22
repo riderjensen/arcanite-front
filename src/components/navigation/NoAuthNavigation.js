0e32edbf6ed9680a770e097c50f872604b27b00f
rider <riderjensen@gmail.com>
2019-12-21 19:28:52 -0700

    SHift some items and remove imports

--- src/components/navigation/NoAuthNavigation.js
+++ src/components/navigation/NoAuthNavigation.js
@@ -1,32 +1,30 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { NavLink } from 'react-router-dom'
 import './NoAuthNavigation.css';
 
-class NoAuthNavigation extends Component {
-    render() {
-        return (
-            <nav className="navbar">
-                <label className="navbar-toggle" id="js-navbar-toggle" htmlFor="chkToggle">
-                    <span>&#8213;</span>
-                    <span>&#8213;</span>
-                    <span>&#8213;</span>
-                </label>
-                <NavLink className="logo" to="/">Home</NavLink>
-                <input type="checkbox" id="chkToggle"></input>
-                <ul className="main-nav" id="js-menu">
-                    <li>
-                        <NavLink to="/about">About</NavLink>
-                    </li>
-                    <li>
-                        <NavLink to="/users">Users</NavLink>
-                    </li>
-                    <li className="right-align sign-up">
-                        <NavLink to="/login">Login</NavLink>
-                    </li>
-                </ul>
-            </nav>
-        )
-    }
+function NoAuthNavigation() {
+    return (
+        <nav className="navbar">
+            <label className="navbar-toggle" htmlFor="chkToggle">
+                <span>&#8213;</span>
+                <span>&#8213;</span>
+                <span>&#8213;</span>
+            </label>
+            <NavLink className="logo" to="/">Home</NavLink>
+            <input type="checkbox" id="chkToggle"></input>
+            <ul className="main-nav">
+                <li>
+                    <NavLink to="/about">About</NavLink>
+                </li>
+                <li>
+                    <NavLink to="/users">Users</NavLink>
+                </li>
+                <li className="right-align sign-up">
+                    <NavLink to="/login">Login</NavLink>
+                </li>
+            </ul>
+        </nav>
+    )
 }
 
 export default NoAuthNavigation;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 
 import NoAuthNavigation from '../components/navigation/NoAuthNavigation';
 

1bfefdcd3251d678bb223432fa7d40338b5b9fcc
Rider Jensen <riderjensen@gmail.com>
2020-01-22 11:11:25 -0700

    Adding post container

--- src/routes/AuthRoutes.js
+++ src/routes/AuthRoutes.js
@@ -3,12 +3,13 @@ import { Route, Redirect, Switch } from 'react-router-dom';
 
 import Home from '../containers/home-container/HomeContainer';
 import DashboardContainer from '../containers/profile-container/ProfileContainer';
+import PostContainer from '../containers/post-container/PostContainer';
 
 function myAuthRouter() {
     return (
         <Switch>
             <Route exact path="/dashboard" component={DashboardContainer} />
-            <Route path="/dashboard/:id" component={DashboardContainer} />
+            <Route path="/:id" component={PostContainer} />
             <Route exact path="/" component={Home} />
             <Redirect to="/" />
         </Switch>
--- src/routes/UnAuthRoutes.js
+++ src/routes/UnAuthRoutes.js
@@ -2,16 +2,19 @@ import React from 'react';
 import { Route, Redirect, Switch } from 'react-router-dom';
 
 import Home from '../containers/home-container/HomeContainer';
-
+import PostContainer from '../containers/post-container/PostContainer';
+import About from '../components/about/About';
 import Login from '../components/auth/Login';
 import SignUp from '../components/auth/SignUp';
 
 function myUnAuthRouter() {
     return (
         <Switch>
-            <Route exact path="/" component={Home} />
+            <Route exact path="/about" component={About} />
             <Route exact path="/login" component={Login} />
             <Route exact path="/signup" component={SignUp} />
+            <Route path="/:id" component={PostContainer} />
+            <Route exact path="/" component={Home} />
             <Redirect to="/" />
         </Switch>
     )

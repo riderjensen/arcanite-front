79ea1e8d8553d4ca361415aa8e22f457afb4e75a
Rider Jensen <riderjensen@gmail.com>
2020-01-22 11:10:46 -0700

    adding an about page

---
+++ src/components/about/About.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import NoAuthNavigation from '../navigation/NoAuthNavigation';
+import './About.css';
+
+function About() {
+    return (
+        <div>
+            <NoAuthNavigation />
+            <h1>About Project Arcanite</h1>
+        </div>
+    )
+}
+
+export default About;

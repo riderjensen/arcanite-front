410eda7638fb95a800639626985510d150e75f59
Rider Jensen <riderjensen@gmail.com>
2020-01-22 09:59:50 -0700

    adding some fake state

--- src/components/profile/Profile.js
+++ src/components/profile/Profile.js
@@ -5,6 +5,29 @@ import './Profile.css';
 
 class Dashboard extends Component {
     state = {
+        information:[
+            {
+                type: "post",
+                content: "My post title",
+                votes: 34,
+                edited: false,
+                _id: 1234
+            },
+            {
+                type: "post",
+                content: "My second post title",
+                votes: 53,
+                edited: true,
+                _id: 12345
+            },
+            {
+                type: "comment",
+                content: "My first comment",
+                votes: 11,
+                edited: true,
+                _id: 123456
+            }
+        ]
     }
 
     render() {

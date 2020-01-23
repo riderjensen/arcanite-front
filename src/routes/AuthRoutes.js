ff97782c6f76e173caa7378a122d0c813a080d59
rider <riderjensen@gmail.com>
2020-01-22 17:39:40 -0700

    Fix route issues

--- src/routes/AuthRoutes.js
+++ src/routes/AuthRoutes.js
@@ -1,12 +1,20 @@
 import React from 'react';
-import { Route, Switch } from 'react-router-dom';
+import { Route, Switch, Redirect } from 'react-router-dom';
 
-import DashboardContainer from '../containers/profile-container/ProfileContainer';
+import ProfileContainer from '../containers/profile-container/ProfileContainer';
+import PostContainer from '../containers/post-container/PostContainer';
+import HomeContainer from '../containers/home-container/HomeContainer';
+import About from '../components/about/About';
+
 
 function myAuthRouter() {
     return (
         <Switch>
-            <Route exact path="/dashboard" component={DashboardContainer} />
+            <Route exact path="/dashboard" component={ProfileContainer} />
+            <Route exact path="/about" component={About} />
+            <Route path="/post/:id" component={PostContainer} />
+            <Route exact path="/" component={HomeContainer} />
+            <Redirect to="/" />
         </Switch>
     )
 }
--- src/routes/UnAuthRoutes.js
+++ src/routes/UnAuthRoutes.js
@@ -1,14 +1,22 @@
 import React from 'react';
-import { Route, Switch } from 'react-router-dom';
+import { Route, Switch, Redirect } from 'react-router-dom';
 
+import PostContainer from '../containers/post-container/PostContainer';
+import HomeContainer from '../containers/home-container/HomeContainer';
 import Login from '../components/auth/Login';
 import SignUp from '../components/auth/SignUp';
+import About from '../components/about/About';
+
 
 function myUnAuthRouter() {
     return (
         <Switch>
             <Route exact path="/login" component={Login} />
             <Route exact path="/signup" component={SignUp} />
+            <Route exact path="/about" component={About} />
+            <Route path="/post/:id" component={PostContainer} />
+            <Route exact path="/" component={HomeContainer} />
+            <Redirect to="/" />
         </Switch>
     )
 }

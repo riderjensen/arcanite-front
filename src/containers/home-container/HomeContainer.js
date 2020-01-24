15765444f1cc8dce019a34187db3a22e401b5b5e
rider <riderjensen@gmail.com>
2020-01-24 15:10:31 -0700

    main homepage

--- src/containers/home-container/HomeContainer.js
+++ src/containers/home-container/HomeContainer.js
@@ -1,21 +1,37 @@
-import React from 'react';
+import React, { Component } from 'react';
+import axios from 'axios';
+import Card from '../../components/card/Card';
 
 import NoAuthNavigation from '../../components/navigation/NoAuthNavigation';
 
 import './HomeContainer.css';
 
 
-function Home() {
-    return (
-        <div>
-            <NoAuthNavigation />
-            <div className="main-header">
-                <p>
-                    Main page for posts to be displayed
-                </p>
+class Home extends Component {
+
+    state = {
+        posts: []
+    }
+
+    componentDidMount() {
+        // check the state for posts first then call if there are none
+        axios.get('http://localhost:8080/a').then(resp => {
+            this.setState({ posts: resp.data });
+        }).catch(err => console.log(err))
+    }
+
+    render() {
+        return (
+            <div>
+                <NoAuthNavigation />
+                <div className="main-header">
+                    {this.state.posts.map(post => (
+                        <Card {...post} key={post._id} />
+                    ))}
+                </div>
             </div>
-        </div>
-    )
+        )
+    }
 }
 
 export default Home;

bf0d15f0871baeec4f029192e4c3eca4c48a09dd
rider <riderjensen@gmail.com>
2020-01-25 16:09:30 -0700

    adding show modal and hide modal functions

--- src/store/actionTypes.js
+++ src/store/actionTypes.js
@@ -1,4 +1,5 @@
 export const LOGIN_USER = "LOGIN_USER"
 export const SIGN_UP_USER = "SIGN_UP_USER"
 export const LOGOUT_USER = "LOGOUT_USER"
-
+export const MODAL_SHOW = "MODAL_SHOW"
+export const MODAL_HIDE = "MODAL_HIDE"
--- src/store/reducers/index.js
+++ src/store/reducers/index.js
@@ -1,18 +1,21 @@
 import * as actionTypes from '../actionTypes';
 
 const initialState = {
-	username: null
+	username: null,
+	modal: false
   }
   
 export default function reducer(state = initialState, action) {
 	switch (action.type) {
-	case actionTypes.LOGIN_USER:
-		return {...state, username: action.payload}
-	case actionTypes.LOGOUT_USER:
-		return {...state, username: null}
-	case actionTypes.SIGN_UP_USER:
-		return {...state, username: action.payload}
-	default:
-		return state;
+		case actionTypes.LOGIN_USER:
+			return {...state, username: action.payload}
+		case actionTypes.LOGOUT_USER:
+			return {...state, username: null}
+		case actionTypes.SIGN_UP_USER:
+			return {...state, username: action.payload}
+		case actionTypes.MODAL_SHOW:
+			return {...state, modal: action.payload}
+		default:
+			return state;
 	}
 }

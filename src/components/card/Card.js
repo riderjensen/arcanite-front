fd6ae77b0a1dab8baa1d24e9848a359b1b412d0b
rider <riderjensen@gmail.com>
2020-01-26 14:30:55 -0700

    uppercase username

--- src/components/card/Card.js
+++ src/components/card/Card.js
@@ -4,11 +4,16 @@ import './Card.css';
 
 function Card(props) {
     return (
+
         <div className="card staff">
             <h5 className="card-title">{props.content} - <span className="title">{props.votes} votes</span></h5>
             <p className="view-link">
                 <NavLink  to={'/post/'+props._id}>View</NavLink>
             </p>
+            {props.user === props.loggedInUser ? <p className="view-link">
+                Edit
+            </p> : null}
+
         </div>
     )
 }

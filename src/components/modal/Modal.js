faf37bd94ecad06e44a8f1e10057ada0c88c600e
rider <riderjensen@gmail.com>
2020-01-26 14:42:59 -0700

    Adding a close to the modal

--- src/components/modal/Modal.js
+++ src/components/modal/Modal.js
@@ -34,6 +34,7 @@ class Modal extends Component {
             <div className="overlay">
                 <div onClick={this.props.passFunction} className="overlay"></div>
                 <div onClick={this.preventIssue} className="modal">
+                    <span className="close" onClick={this.props.passFunction}>X</span>
                     <h1>Create Post</h1>
                     <input type="text" name="content" onChange={this.handleChange} />
                     <button onClick={this.submitPost}>Submit</button>

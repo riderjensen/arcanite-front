ac80b3fc7575a35a00458ede6cd7cfbffc169212
rider <riderjensen@gmail.com>
2020-01-26 14:29:44 -0700

    change authroute to be profile

--- src/routes/AuthRoutes.js
+++ src/routes/AuthRoutes.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Route, Switch, Redirect } from 'react-router-dom';
 
-import ProfileContainer from '../containers/profile-container/ProfileContainer';
+import Profile from '../components/profile/Profile';
 import PostContainer from '../containers/post-container/PostContainer';
 import HomeContainer from '../containers/home-container/HomeContainer';
 import About from '../components/about/About';
@@ -10,7 +10,7 @@ import About from '../components/about/About';
 function myAuthRouter() {
     return (
         <Switch>
-            <Route exact path="/dashboard" component={ProfileContainer} />
+            <Route exact path="/profile" component={Profile} />
             <Route exact path="/about" component={About} />
             <Route path="/post/:id" component={PostContainer} />
             <Route exact path="/" component={HomeContainer} />

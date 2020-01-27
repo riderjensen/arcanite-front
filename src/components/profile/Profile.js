ae637d889b8068e0da5247fd24bd28a9ec40d4a9
rider <riderjensen@gmail.com>
2020-01-26 22:09:51 -0700

    adding posts and comments to profile

--- src/components/profile/Profile.js
+++ src/components/profile/Profile.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import PostCard from '../card/PostCard';
+import CommentCard from '../card/CommentCard';
 
 import * as actions from '../../store/actions/index';
 
@@ -23,9 +24,13 @@ class Profile extends Component {
             <div className="profile">
                 <h1>Profile</h1>
                 <button onClick={this.logOutFunction}>Logout</button>
-                {this.props.userPosts.length > 0 ? this.props.userPosts.map(card => (
-                    <PostCard {...card} loggedInUser={this.props.username} key={card._id} />
-                )) : null}
+                {this.props.userPosts.length > 0 ? this.props.userPosts.map(card => {
+                    if (card.type === "post") {
+                        return <PostCard {...card} loggedInUser={this.props.username} key={card._id} />
+                    } else {
+                        return <CommentCard {...card} loggedInUser={this.props.username} key={card._id} />
+                    }                       
+                }) : null}
             </div>
         )
     }

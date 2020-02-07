7a987b831ccffe779b52043abab17bff3964a0f9
rider <riderjensen@gmail.com>
2020-02-06 18:30:17 -0700

    fix security issue

--- src/components/about/About.js
+++ src/components/about/About.js
@@ -8,7 +8,7 @@ function About() {
             <div class="about-text">
                 <p>Project Arcanite is an new project founded in the spirit of YikYak. We want the ability for people to post things and be able to comment on them but with limited ability. For example, we only want to allow top level comments but we also want to allow editing of posts at least once.</p>
                 <p>Our vision is to create a place where people can talk freely but still have some restrictions on how deep those conversations can go.</p>
-                <p>We are constantly updating this application so please stay tuned. You are welcome to follow it's progress on the <a href="https://github.com/riderjensen/arcanite-front" target="_blank">Github page</a>.</p>
+                <p>We are constantly updating this application so please stay tuned. You are welcome to follow it's progress on the <a href="https://github.com/riderjensen/arcanite-front" target="_blank" rel="noopener noreferrer">Github page</a>.</p>
             </div>
         </div>
     )

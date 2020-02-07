252d7f18a79e9001cbff9e1e2011440fc086d2f5
rider <riderjensen@gmail.com>
2020-02-06 18:21:15 -0700

    Updating the about page to have content

--- src/components/about/About.js
+++ src/components/about/About.js
@@ -5,6 +5,8 @@ function About() {
     return (
         <div>
             <h1>About Project Arcanite</h1>
+            <p>Project Arcanite is an new project founded in the spirit of YikYak. We want the ability for people to post things and be able to comment on them but with limited ability. For example, we only want to allow top level comments but we also want to allow editing of posts at least once.</p>
+            <p>Our vision is to create a place where people can talk freely but still have some restrictions on how deep those conversations can go.</p>
         </div>
     )
 }

270b95e513138c235ae4ba6f358de7675baedb93
rider <riderjensen@gmail.com>
2020-02-09 20:28:17 -0700

    Adding a counter for the text box

--- src/components/modal/Modal.js
+++ src/components/modal/Modal.js
@@ -2,10 +2,12 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faTimes } from '@fortawesome/free-solid-svg-icons';
+import { CircularProgressbar } from 'react-circular-progressbar';
 
 import Spinner from '../spinner/Spinner';
 
 import './Modal.css'
+import 'react-circular-progressbar/dist/styles.css';
 
 import * as actions from '../../store/actions/index';
 
@@ -42,10 +44,11 @@ class Modal extends Component {
             <div className="overlay">
                 <div onClick={this.props.passFunction} className="overlay"></div>
                 <div onClick={this.preventIssue} className="modal">
+                    <CircularProgressbar className={this.state.content.length > 250 ? 'svgerror' : null} value={this.state.content.length} maxValue={250} minValue={0} strokeWidth={25} />
                     <FontAwesomeIcon className="close" onClick={this.props.passFunction} icon={faTimes}></FontAwesomeIcon>
                     <h1>Create Post</h1>
                     <textarea type="text" name="content" onChange={this.handleChange} rows="3" />
-                    {this.state.submitting ? <div style={{width: '100%'}}><Spinner /></div>: <button onClick={this.submitPost}>Submit</button>}
+                    {this.state.submitting ? <div style={{width: '100%'}}><Spinner /></div>: <button disabled={this.state.content.length > 250 ? true : false} onClick={this.submitPost}>Submit</button>}
                 </div>
             </div>
         )

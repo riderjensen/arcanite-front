4c7154a8e68dac9b50df2389eec4efcc01dc4ebe
rider <riderjensen@gmail.com>
2020-02-12 21:16:04 -0700

    Adding comments to the home

--- src/containers/home-container/HomeContainer.js
+++ src/containers/home-container/HomeContainer.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 
 import PostCard from '../../components/card/PostCard';
+import CommentCard from '../../components/card/CommentCard';
 import Spinner from '../../components/spinner/Spinner';
 
 import * as actions from '../../store/actions/index';
@@ -19,14 +20,19 @@ class Home extends Component {
 
     render() {
         return (
-            <div>
-                <div className="main-header">
+            <div className="main-header">
+                <div className="post-area">
                     {this.props.posts.length > 0 ? this.props.posts.map(post => (
                         <PostCard {...post} loggedInUser={this.props.username} key={post._id} />
                     )) : <div>
                             <p className="load-warning">Please be patient while our backend wakes up.</p>
                             <Spinner />
                         </div>}
+                </div>
+                <div className="comment-area">
+                    {this.props.selectedPost ? this.props.selectedPost.comments ? this.props.selectedPost.comments.length > 0 ? this.props.selectedPost.comments.map(comment => {
+                        return <CommentCard {...comment} loggedInUser={this.props.username} key={comment._id} />
+                    }) : null: null : null}
                 </div>
             </div>
         )
@@ -36,7 +42,8 @@ class Home extends Component {
 const mapStateToProps = state => {
     return {
         posts: state.index.posts,
-        username: state.index.username
+        username: state.index.username,
+        selectedPost: state.index.selectedPost
     }
 }
 
